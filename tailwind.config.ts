24ff12db25aaec6bbd1e7d8d51301d594b7de87c
hadiasta <hijrihadi@gmail.com>
2025-10-18 18:03:22 +0700

    feat : change theme to light init login form fix config bg color from tailwind.config to global @theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         nunito: ['var(--font-nunito)', 'sans-serif'],
       },
-      colors: {
-        greenPrimary: '#01959F',
-      },
     },
   },
 }
 
-export default config;
+export default config
