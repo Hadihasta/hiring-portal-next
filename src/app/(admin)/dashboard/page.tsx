b56421da97df1852518e4ee2fe71cf7b52ba741b
hadiasta <hijrihadi@gmail.com>
2025-10-19 10:22:55 +0700

    fix : error hydration  with deleting mutiple html tag and body tag

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from '@/components/global/Navbar'
+
 
 const page = () => {
   return (
     <div>
-      <Navbar  label={'Job List'} src='/asset/global/TemplateAdmin.jpg'/>
+    admin
     </div>
   )
 }
