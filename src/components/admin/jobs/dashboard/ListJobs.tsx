d91bc72d74e81d1a6c518f1e875445e4f0a43cf8
hadiasta <hijrihadi@gmail.com>
2025-10-20 17:43:02 +0700

    feat : add llist job list get id when click manage jobs

--- src/components/admin/jobs/dashboard/ListJobs.tsx
+++ src/components/admin/jobs/dashboard/ListJobs.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import Button from '@/components/global/Button'
+import { getJobNumber } from '@/lib/getJobNumber'
 
 interface SalaryRange {
   min: number
@@ -36,11 +37,7 @@ const statusBadgeColor: Record<string, string> = {
 
 const ListJobs: React.FC<ListJobsProps> = ({ data }) => {
   if (!data || data.length === 0) {
-    return (
-      <div className="flex flex-col justify-center items-center py-12 text-gray-500">
-        No job openings found.
-      </div>
-    )
+    return <div className="flex flex-col justify-center items-center py-12 text-gray-500">No job openings found.</div>
   }
 
   return (
@@ -48,26 +45,31 @@ const ListJobs: React.FC<ListJobsProps> = ({ data }) => {
       {data.map((job) => (
         <div
           key={job.id}
-          className="w-full bg-white rounded-2xl shadow-sm p-4 flex justify-between items-center border border-gray-100"
+          className="w-full bg-white p-[24px] rounded-2xl shadow-sm  flex justify-between items-end border border-gray-100"
         >
           <div className="flex flex-col gap-1">
             <div className="flex items-center gap-2">
               <span
-                className={`px-3 py-[2px] text-xs font-medium rounded-full ${statusBadgeColor[job.status.toLowerCase()] ?? ''}`}
+                className={`px-[16px] py-[4px] text-xs font-medium rounded-lg ${
+                  statusBadgeColor[job.status.toLowerCase()] ?? ''
+                }`}
               >
                 {job.list_card.badge}
               </span>
-              <span className="text-xs text-gray-400">{job.list_card.started_on_text}</span>
+              <span className="text-xs text-greyNeutral border border-greyBorder px-[16px] py-[4px] rounded-sm ">
+                {job.list_card.started_on_text}
+              </span>
             </div>
 
-            <h3 className="text-lg font-semibold">{job.title}</h3>
-            <p className="text-gray-600 text-sm">{job.salary_range.display_text}</p>
+            <h3 className="text-lg font-bold">{job.title}</h3>
+            <p className="text-greyText  text-base ">{job.salary_range.display_text}</p>
           </div>
 
           <Button
             color="green"
             label={job.list_card.cta}
-            onClick={() => console.log('Manage job clicked', job.id)}
+            className="text-white text-xs rounded-lg"
+            onClick={() => console.log('Manage job clicked', getJobNumber(job.id))}
           />
         </div>
       ))}
