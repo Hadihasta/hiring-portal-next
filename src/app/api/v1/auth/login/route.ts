c32e3c634f167bfef3b988af96d10b6d9a41ea37
hadiasta <hijrihadi@gmail.com>
2025-10-22 03:59:18 +0700

    feat: debuging on jwt credential

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs'
+
 import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { signJwt } from '@/lib/jwt'
@@ -5,39 +7,27 @@ import { signJwt } from '@/lib/jwt'
 export async function POST(req: Request) {
   try {
     const { email, password } = await req.json()
-
-    // Cek apakah email & password diisi
     if (!email || !password) {
       return NextResponse.json({ error: 'Email and password are required' }, { status: 400 })
     }
 
-    // Cari user berdasarkan email
-    const user = await prisma.users.findUnique({
-      where: { email },
-    })
-
-    // Jika user tidak ditemukan
+    const user = await prisma.users.findUnique({ where: { email } })
     if (!user) {
       return NextResponse.json({ error: 'Invalid credentials Email not Found' }, { status: 401 })
     }
 
-    // Bandingkan password secara langsung (karena tidak di-hash)
     if (user.password !== password) {
       return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
     }
 
-    // Buat token JWT
-    const token = signJwt({
-      email: user.email,
-      password: user.password,
-    })
+    console.log("🔑 JWT_SECRET :", !!process.env.JWT_SECRET)
 
-    // Response sukses
+    const token = signJwt({ email: user.email, password: user.password })
+
     return NextResponse.json({
       message: 'Login successful',
       token,
       data: {
-        // bigint tidak bisa di-serialize ke JSON, jadi di-convert ke number
         id: Number(user.id),
         email: user.email,
         name: user.name,
